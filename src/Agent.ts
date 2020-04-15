d88ca729c44f74fc214b92c892eecf4c7eaa96d3
Alex Jeffrey <ajeffrey@williamhill.co.uk>
2020-04-15 16:31:31 +0100

    made commands into objects

--- src/Agent.ts
+++ src/Agent.ts
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 import Human from './Human';
-import { ICommandStep, idle, interrupt } from './commands';
+import { ICommand, idle, interrupt } from './commands';
 
 export default class Agent {
   private isDying: boolean;
-  private command: ICommandStep;
+  private command: ICommand;
 
   constructor(private human: Human) {
     this.isDying = false;
@@ -15,6 +15,6 @@ export default class Agent {
     this.human.hunger -= dt * 5;
     this.human.step();
 
-    this.command = interrupt(this.human)(() => this.command(dt));
+    this.command = interrupt(this.human)(() => this.command.step(dt));
   }
 }
